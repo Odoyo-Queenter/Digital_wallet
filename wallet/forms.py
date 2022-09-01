cb0e31268651294f16eaa40892499bdf4a0732d2
Odoyo-Queenter <achiengodoyoqueenter@gmail.com>
2022-09-01 22:16:47 +0300

    forms

--- wallet/forms.py
+++ wallet/forms.py
@@ -1,5 +1,6 @@
+from dataclasses import fields
 from django import forms
-from .models import Customer 
+from .models import Account, Card, Customer, Loan, Notification, Receipt, Reward, ThirdParty, Transaction, Wallet 
 
 
 class CustomerRegistrationForm(forms.ModelForm):
@@ -7,5 +8,50 @@ class CustomerRegistrationForm(forms.ModelForm):
         model = Customer
         fields ='__all__'
 
+class AccountRegistrationForm(forms.ModelForm):
+    class Meta:
+        model = Account
+        fields = '__all__'
+
+class WalletRegistrationForm(forms.ModelForm):
+    class Meta:
+        model = Wallet
+        fields = '__all__'
+
     
-    
+class TransactionRegistrationForm(forms.ModelForm):
+    class Meta:
+        model = Transaction
+        fields = '__all__'
+
+class CardRegistrationForm(forms.ModelForm):
+    class Meta:
+        model = Card
+        fields = '__all__'
+
+class ThirdPartyRegistrationForm(forms.ModelForm):
+    class Meta:
+        model = ThirdParty
+        fields = '__all__'
+
+class ReceiptRegistrationForm(forms.ModelForm):
+    class Meta:
+        model = Receipt
+        fields = '__all__'
+
+
+class NotificationRegistrationForm(forms.ModelForm):
+    class Meta:
+        model = Notification
+        fields = '__all__'
+
+class LoanRegistrationForm(forms.ModelForm):
+    class Meta:
+        model = Loan
+        fields = '__all__'
+
+class RewardRegistrationForm(forms.ModelForm):
+    class Meta:
+        model = Reward
+        fields = '__all__'
+
--- wallet/urls.py
+++ wallet/urls.py
@@ -1,6 +1,16 @@
 from django.urls import path
-from .views import register_customer
+from .views import register_account, register_card, register_customer, register_loan, register_notification, register_receipt, register_reward, register_thirdparty, register_transaction, register_wallet
 
 urlpatterns = [
     path("register/",register_customer,name ="registration"),
+    path("account/",register_account,name ="account"),
+    path("wallet/",register_wallet,name ="wallet"),
+    path("transaction/",register_transaction,name ="transaction"),
+    path("card/",register_card,name ="card"),
+    path("thirdparty/",register_thirdparty,name ="thirdparty"),
+    path("receipt/",register_receipt,name ="receipt"),
+    path("notification/",register_notification,name ="notification"),
+    path("loan/",register_loan,name ="loan"),
+    path("reward/",register_reward,name ="reward"),
+
 ]
